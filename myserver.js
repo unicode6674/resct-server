a872871b74f772220d3464bd547b65e424662153
unicode6674 <18837028037@163.com>
2019-12-30 18:49:34 +0800

    denglu

--- myserver.js
+++ myserver.js
@@ -1,13 +1,30 @@
+const http = require('http');
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');//引入body parser用于解析post的body
+
+app.use(bodyParser.json());//使用body parser用于解析post的body
+app.use(bodyParser.urlencoded({ extended: true }));//使用body parser用于解析post的body
+
+// app.all('*', function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "X-Requested-With");
+//     res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+//     res.header("X-Powered-By",' 3.2.1');
+//     res.header("Content-Type", "application/json;charset=utf-8");
+//     next();
+// });
+
+// 加 登录后
 app.all('*', function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With,Content-Type,Access-Token");
     res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
     res.header("X-Powered-By",' 3.2.1');
     res.header("Content-Type", "application/json;charset=utf-8");
     next();
 });
+
 app.get('/', function (req, res) {
     res.send('This is test message!');
 });
@@ -20,6 +37,19 @@ app.get('/json', function (req, res) { //添加的代码
     };
     res.send(myjson);
 });
+
+app.use(express.static('public'));
+
+app.post('/password', function (req, res) {  //接收POST请求
+    let data = req.body;   //解析body中的信息
+    console.log(data);
+    let message1 = {success:true};
+    let message2 = {success:false};
+    if(data.user==='hjh'&&data.password==='123456'){ //判断并返回结果
+        res.send(message1);
+    }
+    else res.send(message2);
+})
 
 var server = app.listen(8081, function () {
     var host = server.address().address;
